bf44ad5c180941151519af94fb888a171f8ff999
Vanessa Viray <13virayvanessab@gmail.com>
2024-05-02 17:27:50 +0000

    Generate a DOM tree. #1

--- ts/main.ts
+++ ts/main.ts
@@ -9,12 +9,16 @@ const $marketmonLogo = document.querySelector(
   '#marketmon-logo',
 ) as HTMLImageElement;
 const $searchInput = document.querySelector('#search-input') as HTMLFormElement;
+const $cardContainerRow = document.querySelector(
+  '#card-container-row',
+) as HTMLDivElement;
 
 if (!$form) throw new Error('the $from query failed.');
 if (!$welcomePage) throw new Error('the $welcomePage query failed');
 if (!$searchResults) throw new Error('the $searchResults query failed');
 if (!$marketmonLogo) throw new Error('the $marketmonLogo query failed');
 if (!$searchInput) throw new Error('the $searchInput query failed.');
+if (!$cardContainerRow) throw new Error('the $cardContainerRow query failed.');
 
 // EVENT LISTENER: to listen for when the MarketMon logo is clicked
 
@@ -57,29 +61,61 @@ async function fetchCards(searchCriteria: unknown): Promise<void> {
       throw new Error('Network response was not ok');
     }
     const cardObjects = await response.json();
-    const cardObjectsData = cardObjects.data;
-    const cardInfoArray = [];
 
-    for (let i = 0; i < cardObjectsData.length; i++) {
-      const cardInfo: any = {};
-
-      const smallImage = cardObjects.data[i].images.small;
-      cardInfo.smallImage = smallImage;
-
-      const cardName = cardObjects.data[i].name;
-      cardInfo.cardName = cardName;
-
-      const setName = cardObjects.data[i].set.name;
-      cardInfo.setName = setName;
-
-      const cardNumber = cardObjects.data[i].number;
-      cardInfo.cardNumber = cardNumber;
-
-      cardInfoArray.push(cardInfo);
-    }
-    console.log('Pokemon Data: ', cardObjects);
-    console.log(cardInfoArray);
+    renderCards(cardObjects);
+    // console.log('Pokemon Data: ', cardObjects);
   } catch (error) {
     console.log('Fetch function failed: ', error);
   }
 }
+
+// FUNCTION: to render cards
+
+function renderCards(cardObjects: any): any {
+  // get card info array
+  const cardObjectsData = cardObjects.data;
+  const cardInfoArray: any = [];
+
+  for (let i = 0; i < cardObjectsData.length; i++) {
+    const cardInfo: any = {};
+
+    const smallImage = cardObjects.data[i].images.small;
+    const cardName = cardObjects.data[i].name;
+    const setName = cardObjects.data[i].set.name;
+    const cardNumber = cardObjects.data[i].number;
+
+    cardInfo.smallImage = smallImage;
+    cardInfo.cardName = cardName;
+    cardInfo.setName = setName;
+    cardInfo.cardNumber = cardNumber;
+
+    cardInfoArray.push(cardInfo);
+
+    // render the dom tree
+
+    const $cardContainer = document.createElement('div');
+    $cardContainer.setAttribute('class', 'card-container');
+    $cardContainerRow.appendChild($cardContainer);
+
+    const $imageElement = document.createElement('img');
+    $imageElement.setAttribute('src', `${cardInfo.smallImage}`);
+    $cardContainer.appendChild($imageElement);
+
+    const $cardNameH4Element = document.createElement('h4');
+    $cardNameH4Element.textContent = cardInfo.cardName;
+    $cardContainer.appendChild($cardNameH4Element);
+
+    const $setNamePElement = document.createElement('p');
+    $setNamePElement.textContent = cardInfo.setName;
+    $cardContainer.appendChild($setNamePElement);
+
+    const $cardNumberPElement = document.createElement('p');
+    $cardNumberPElement.textContent = cardInfo.cardNumber;
+    $cardContainer.appendChild($cardNumberPElement);
+
+    console.log($cardContainerRow);
+  }
+  console.log(cardInfoArray);
+}
+
+// DOM TREE VISUAL:
